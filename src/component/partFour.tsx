de15e640685091ac976d9183ca47aa5733bf391f
yuyunzhi <lambert1129@foxmail.com>
2021-06-07 09:39:12 +0800

    feat: update ui

--- src/component/partFour.tsx
+++ src/component/partFour.tsx
@@ -43,9 +43,9 @@ const PartFour: FC<IPartFour> = (props) => {
                     }}
                     onBlur={(value) => console.log(`onBlur: ${value}`)}
                 />
-              <span>年</span>
+                <span>年</span>
             </Cell>
-            <Cell title="年化收益率 %">
+            <Cell title="年化收益率">
                 <Input
                     clearable
                     type="text"
